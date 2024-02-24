54f07dd6c5dcad3e3386d44957f5f43e626ceabf
jrundquist <jrscienceguy@gmail.com>
2024-02-23 16:28:17 -0800

    Transitions can have some number of spaces after the :

--- screenFormatPlugin/FountainRegex.ts
+++ screenFormatPlugin/FountainRegex.ts
@@ -1,7 +1,8 @@
 export const SCENE_HEADER_PATTERN = /(?:\n|^)((int|ext|est|i\.?\/e\.?|e\.?\/i\.?)($|\s|\.|\-))/gi;
 export const SCENE_NUMBER_PATTERN = /^\s*\#?[0-9][a-z0-9\.\-\(\)]*\s*$/;
 
-export const TRANSITION_PATTERN = /(?:\n|^)(?:FADE|CUT|TRANSITION)\s*(?:TO\s*)?(?:[^<>\na-z]*):$/;
+export const TRANSITION_PATTERN =
+  /(?:\n|^)(?:FADE|CUT|TRANSITION)\s*(?:TO\s*)?(?:[^<>\na-z]*):\s*$/;
 
 export const CHARACTER_PATTERN = /^[A-Z0-9][A-Z0-9\s\_\-\(\)\.]{2,}\:?$/;
 
